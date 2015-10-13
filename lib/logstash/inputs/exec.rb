ab1c301a3e04274a7d2561e0cbd33588037aece0
guyboertje <guyboertje@gmail.com>
2015-10-13 18:24:40 +0100

    refactor code and tests to make test more reliable

--- lib/logstash/inputs/exec.rb
+++ lib/logstash/inputs/exec.rb
@@ -33,23 +33,25 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
   end # def register
 
   def run(queue)
-
     while !stop?
-      start = Time.now
-      execute(@command)
-      duration = Time.now - start
-
-      @logger.info? && @logger.info("Command completed", :command => @command, :duration => duration)
-
-      wait_until_end_of_interval(duration)
+      inner_run(queue)
     end # loop
   end # def run
 
+  def inner_run(queue)
+    start = Time.now
+    execute(@command, queue)
+    duration = Time.now - start
+
+    @logger.info? && @logger.info("Command completed", :command => @command, :duration => duration)
+
+    wait_until_end_of_interval(duration)
+  end
+
   def stop
-    if @io
-      @io.close
-      @io = nil
-    end
+    return if @io.nil? || @io.closed?
+    @io.close
+    @io = nil
   end
 
   private
@@ -60,17 +62,18 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
     # Sleep for the remainder of the interval, or 0 if the duration ran
     # longer than the interval.
     sleeptime = [0, @interval - duration].max
-    if sleeptime == 0
+    if sleeptime > 0
+      Stud.stoppable_sleep(sleeptime) { stop? }
+    else
       @logger.warn("Execution ran longer than the interval. Skipping sleep.",
                    :command => @command, :duration => duration, :interval => @interval)
-    else
-      Stud.stoppable_sleep(sleeptime) { stop? }
     end
   end
 
   # Execute a given command
   # @param [String] A command string
-  def execute(command)
+  # @param [Array or Queue] A queue to append events to
+  def execute(command, queue)
     @logger.info? && @logger.info("Running exec", :command => command)
     begin
       @io = IO.popen(command)
@@ -80,12 +83,14 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
         event["command"] = command
         queue << event
       end
+    rescue StandardError => e
+      @logger.error("Error while running command",
+        :command => command, :e => e, :backtrace => e.backtrace)
     rescue Exception => e
-      @logger.error("Exception while running command", :e => e, :backtrace => e.backtrace)
+      @logger.error("Exception while running command",
+        :command => command, :e => e, :backtrace => e.backtrace)
     ensure
-      @io.close
-      @io = nil
+      stop
     end
   end
-
 end # class LogStash::Inputs::Exec
--- spec/inputs/exec_spec.rb
+++ spec/inputs/exec_spec.rb
@@ -4,20 +4,45 @@ require_relative "../spec_helper"
 describe LogStash::Inputs::Exec do
 
   it "should register" do
-    input = LogStash::Plugin.lookup("input", "exec").new("command" => "uptime", "interval" => 0)
+    input = LogStash::Plugin.lookup("input", "exec").new("command" => "ls", "interval" => 0)
 
     # register will try to load jars and raise if it cannot find jars or if org.apache.log4j.spi.LoggingEvent class is not present
     expect {input.register}.to_not raise_error
   end
 
+  context "when operating normally" do
+    let(:input) { LogStash::Plugin.lookup("input", "exec").new("command" => "ls", "interval" => 0) }
+    let(:queue) { [] }
+    let(:loggr) { double('loggr') }
+
+    before do
+      allow(loggr).to receive(:info)
+      allow(loggr).to receive(:info?)
+      allow(loggr).to receive(:warn)
+      allow(loggr).to receive(:warn?)
+      allow(loggr).to receive(:debug)
+      allow(loggr).to receive(:debug?)
+    end
+
+    it "enqueues some events" do
+      input.logger = loggr
+      input.register
+      expect(loggr).not_to receive(:error)
+
+      input.inner_run(queue)
+
+      expect(queue.size).not_to be_zero
+    end
+  end
+
   context "when interrupting the plugin" do
 
     it_behaves_like "an interruptible input plugin" do
-      let(:config) { { "command" => "uptime", "interval" => 0 } }
+      let(:config) { { "command" => "ls", "interval" => 0 } }
     end
 
     it_behaves_like "an interruptible input plugin" do
-      let(:config) { { "command" => "uptime", "interval" => 100 } }
+      let(:config) { { "command" => "ls", "interval" => 100 } }
     end
 
   end
